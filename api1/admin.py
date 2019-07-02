62a1af7830a24b965c411d6f8972bacde11749aa
priyanka sharma <priyanka95sharma24@gmail.com>
2019-07-02 10:00:30 +0530

    first commit

--- api1/admin.py
+++ api1/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from info.models import Student,Teacher,Department, StudentFee,TecaherInfo,AssignTeacher
+admin.site.register(Student)
+admin.site.register(StudentFee)
+admin.site.register(Department)
+admin.site.register(Teacher)
+admin.site.register(TecaherInfo)
 # Register your models here.
